2c2e55b56a017e11082e4e9b43461c607ffa52af
Vlad Zaynchkovsly (mac) <zvsx001@gmail.com>
2020-04-28 18:24:14 +0300

    add chai friendly plugin

--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
-
 module.exports = {
-  plugins: [
-    'mocha',
-  ],
+  plugins: ['mocha', 'chai-expect', 'chai-friendly'],
   extends: [
     '@reaction-eslint/core',
     'plugin:mocha/recommended',
+    'plugin:chai-friendly/recommended',
+    'plugin:chai-expect/recommended',
   ],
   rules: {
-    "mocha/no-mocha-arrows": "off",
-    "mocha/no-setup-in-describe": 0,
-    "import/no-extraneous-dependencies": ["error", {"devDependencies": ["**/*.test.ts"]}]
+    'mocha/no-mocha-arrows': 'off',
+    'mocha/no-setup-in-describe': 0,
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.ts'] }],
   },
   globals: {
     __DEV__: true,
